b5a79857f02360aad3be7a8990a52c774712cbc4
senaliadvp66 <senalia888@gmail.com>
2025-04-17 18:22:15 +0800

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import model.*;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        List<User> users = new ArrayList<>();
+        users.add(new Admin("admin", "admin123"));
+        users.add(new Customer("user1", "pass1"));
+
+        System.out.println("===== PROGRAM INVESTASI =====");
+        System.out.print("Username: ");
+        String inputUser = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String inputPass = scanner.nextLine();
+
+        User loggedInUser = null;
+
+        for (User user : users) {
+            if (user.login(inputUser, inputPass)) {
+                loggedInUser = user;
+                break;
+            }
+        }
+
+        if (loggedInUser != null) {
+            System.out.println("Login berhasil sebagai: " + loggedInUser.getRole());
+
+        } else {
+            System.out.println("Login gagal. Username atau password salah.");
+        }
     }
 }
