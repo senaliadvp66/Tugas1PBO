85291543778c32851a5cfbce76d70a44638f6c8a
senaliadvp66 <senalia888@gmail.com>
2025-04-17 18:19:44 +0800

    add admin

---
+++ src/model/Admin.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Admin extends User {
+
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "admin";
+    }
+}
