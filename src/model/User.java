c7132138a7801825ce003db373a0cf97f3857156
senaliadvp66 <senalia888@gmail.com>
2025-04-17 18:17:58 +0800

    add user

---
+++ src/model/User.java
@@ -0,0 +1,21 @@
+package model;
+
+public abstract class User {
+    protected String username;
+    protected String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean login(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
+    public abstract String getRole(); // untuk tahu admin atau customer
+}
