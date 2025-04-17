0ef1ce8b772abe84530e3f244a1d4a6e5ce53225
senaliadvp66 <senalia888@gmail.com>
2025-04-17 18:21:03 +0800

    add cust

---
+++ src/model/Customer.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Customer extends User {
+
+    public Customer(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "customer";
+    }
+}
