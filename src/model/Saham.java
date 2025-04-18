607c6e4ba460c133cea5fa98baea24f4e50be45f
senaliadvp66 <senalia888@gmail.com>
2025-04-18 18:56:11 +0800

    membuat file saham

---
+++ src/model/Saham.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Saham {
+    private String kode;
+    private String namaPerusahaan;
+    private double harga;
+
+
+    public Saham(String kode, String namaPerusahaan, double harga) {
+        this.kode = kode;
+        this.namaPerusahaan = namaPerusahaan;
+        this.harga = harga;
+    }
+
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getNamaPerusahaan() {
+        return namaPerusahaan;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    @Override
+    public String toString() {
+        return kode + " - " + namaPerusahaan + " (Rp" + harga + ")";
+    }
+}
