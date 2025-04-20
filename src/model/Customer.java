a3acd83058925902185efe731c7064d85b93d37a
senaliadvp66 <senalia888@gmail.com>
2025-04-20 16:09:27 +0800

    update fitur customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,9 +1,14 @@
 package model;
 
 public class Customer extends User {
+    private Portofolio portofolio = new Portofolio();
 
     public Customer(String username, String password) {
         super(username, password);
+    }
+
+    public Portofolio getPortofolio() {
+        return portofolio;
     }
 
     @Override
